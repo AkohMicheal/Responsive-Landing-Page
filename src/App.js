446b5e97d0e1f08838b1421e7d573e2308dae0d6
AkohTech <akohmicheal@gmail.com>
2025-06-06 16:05:40 +0100

    commit for changes made to the landing page

--- src/App.js
+++ src/App.js
@@ -12,17 +12,19 @@ import CourseDetails from './Pages/Courses/CourseDetails'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/about" element={<Layout><About /></Layout>} />
-        <Route path="/contact" element={<Layout><Contact /></Layout>} />
-        <Route path="/blog" element={<Layout><Blog /></Layout>} />
-        <Route path="/blog/:id" element={<Layout><BlogDetails /></Layout>} />
-        <Route path="/courses" element={<Layout><Courses /></Layout>} />
-        <Route path="/courses/:slug" element={<Layout><CourseDetails /></Layout>} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          <Route path="/about" element={<Layout><About /></Layout>} />
+          <Route path="/contact" element={<Layout><Contact /></Layout>} />
+          <Route path="/blog" element={<Layout><Blog /></Layout>} />
+          <Route path="/blog/:id" element={<Layout><BlogDetails /></Layout>} />
+          <Route path="/courses" element={<Layout><Courses /></Layout>} />
+          <Route path="/courses/:slug" element={<Layout><CourseDetails /></Layout>} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
